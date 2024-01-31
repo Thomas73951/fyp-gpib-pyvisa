85fb0b2f06809f77b659ca30ae44943c1d2e38ef
Thomas Sharratt <thomassharratt@hotmail.co.uk>
2024-01-31 12:07:03 +0000

    improved plotting and saving of result
    folder based

--- plot_result.m
+++ plot_result.m
@@ -3,14 +3,16 @@ clear all
 close all
 
 SAVE_IMG = true;
-FILE_NAME = "data_1601.csv";
+FILE_NAME = "data_801_MEAS1.csv";
+FOLDER_NAME = "1"; % file ^ in this folder
 
-data = csvread(FILE_NAME)
+
+data = csvread([FOLDER_NAME, filesep, FILE_NAME])
 
 figure()
-f = linspace(10,100, 1601)*1e6;
+f = linspace(5,20, 801)*1e6;
 
-plot(f/1e6, data, 'DisplayName', 'S21')
+plot(f/1e6, data, 'DisplayName', 'S11')
 xlabel("Frequency [MHz]")
 ylabel("Scattering Parameter [dB]")
 xlim([min(f), max(f)]/1e6)
@@ -18,5 +20,5 @@ legend()
 grid on
 
 if (SAVE_IMG)
-  saveImages(["images", filesep])
+  saveImages([FOLDER_NAME, filesep])
 endif
--- save_result.py
+++ save_result.py
@@ -3,18 +3,20 @@ Script to interface with https://github.com/xyphro/UsbGpib
 Specifically HP8711C RF network analyser.
 
 Designed for retreiving sweep data as file
-ASSUMES MEAS2 IS S21, MEAS 1 IS S11
 """
 
+import os
+from pathlib import Path
 import pyvisa
 import visafn
 
 # setup
-START_FREQ = 10e6 # in Hz
-STOP_FREQ = 100e6 # in Hz
-NUM_POINTS = 1601 # max 1601
+START_FREQ = 5e6 # in Hz
+STOP_FREQ = 20e6 # in Hz
+NUM_POINTS = 801 # max 1601
 SAVE_MEAS1 = True
-SAVE_MEAS2 = True
+SAVE_MEAS2 = False
+FOLDER_NAME = "1"
 PRECISION = 5 # in significant digits
 DEVICE = "USB0::1003::8293::_HEWLETT-PACKARD_8711C_US36360157_C.04.52::0::INSTR"
 # device found with pyvisa-shell, list instead of list_resources()
@@ -25,6 +27,7 @@ inst = rm.open_resource(DEVICE)
 filename_base = f"data_{NUM_POINTS}"
 print("################")
 
+Path(FOLDER_NAME).mkdir(parents=True, exist_ok=True)
 
 visafn.query_ID(inst)
 visafn.set_freq_start_stop(inst, START_FREQ, STOP_FREQ)
@@ -35,12 +38,12 @@ inst.write(f"FORM:DATA ASC,{PRECISION}")
 
 if (SAVE_MEAS1):
     data = inst.query("TRAC? CH1FDATA")
-    with open(filename_base + "_MEAS1.csv", 'w') as file:
+    with open(FOLDER_NAME + os.path.sep + filename_base + "_MEAS1.csv", 'w') as file:
         file.write(data)
 
 if (SAVE_MEAS2):
     data = inst.query("TRAC? CH2FDATA")
-    with open(filename_base + "_MEAS2.csv", 'w') as file:
+    with open(FOLDER_NAME + os.path.sep + filename_base + "_MEAS2.csv", 'w') as file:
         file.write(data)
 
 visafn.resume_sweep(inst)
