3c0c0823f9b01bb35acda2a54ae3f16dd15c8867
Thomas Sharratt <thomassharratt@hotmail.co.uk>
2024-02-17 16:51:25 +0000

    hfield - linear results plotting

--- plot_hfield_multi.m
+++ plot_hfield_multi.m
@@ -1,13 +1,13 @@
 clear all
 close all
 
-SAVE_IMG = false;
+SAVE_IMG = true;
+LOG_PLOT = false;
 FILE_NAME = "xsweep_0_40_21.csv";
-##FOLDER_NAME = ["Coil A";
-##               "Coil C"];
-FOLDER_NAME = ["Coil C";
-               "Coil C 2"];
- % file ^ in this folder
+FOLDER_NAME = ["Coil A";
+               "Coil C"];
+##FOLDER_NAME = ["Coil C";
+##               "Coil C 2"];
 
 
 sweepDetailsCell = strsplit(strtok(FILE_NAME,'.'),'_');
@@ -26,11 +26,23 @@ hold on
 
 for i = 1:size(FOLDER_NAME,1)
   data = csvread([strtrim(FOLDER_NAME(i,:)),filesep, "Hfield", filesep, FILE_NAME]);
-  plot(x, data, '-x', 'DisplayName', FOLDER_NAME(i,:))
+
+  if (LOG_PLOT)
+    % Log plot
+  ##  plot(x, data, '-x', 'DisplayName', FOLDER_NAME(i,:))
+  else
+    % Lin plot
+    plot(x, power(10, data/20), '-x', 'DisplayName', FOLDER_NAME(i,:))
+  endif
 endfor
 
 xlabel("x [mm]")
-ylabel("Transmission (S_{21}) representing H field [dB]")
+if (LOG_PLOT)
+  ylabel("Transmission (S_{21}) representing H field [dB]")
+else
+  ylabel("Transmission (S_{21}) representing H field - Linearised")
+endif
+title("Plot of H Field strength for different probe positions (y ~ 0 mm, z ~5 mm)")
 grid on
 legend('FontSize',11)
 
