7e229878f407ae18f09b125afda2f4084578450b
Thomas Sharratt <thomassharratt@hotmail.co.uk>
2024-02-29 12:10:36 +0000

    plot_hfield_multi removed redundant log plot option

--- plot_hfield_multi.m
+++ plot_hfield_multi.m
@@ -1,8 +1,7 @@
 clear all
 close all
 
-SAVE_IMG = true;
-LOG_PLOT = false;
+SAVE_IMG = false;
 FILE_NAME = "xsweep_0_40_21.csv";
 FOLDER_NAME = ["Coil A";
                "Coil C"];
@@ -27,21 +26,11 @@ hold on
 for i = 1:size(FOLDER_NAME,1)
   data = csvread([strtrim(FOLDER_NAME(i,:)),filesep, "Hfield", filesep, FILE_NAME]);
 
-  if (LOG_PLOT)
-    % Log plot
-  ##  plot(x, data, '-x', 'DisplayName', FOLDER_NAME(i,:))
-  else
-    % Lin plot
-    plot(x, power(10, data/20), '-x', 'DisplayName', FOLDER_NAME(i,:))
-  endif
+  plot(x, power(10, data/20), '-x', 'DisplayName', FOLDER_NAME(i,:))
 endfor
 
 xlabel("x [mm]")
-if (LOG_PLOT)
-  ylabel("Transmission (S_{21}) representing H field [dB]")
-else
-  ylabel("Transmission (S_{21}) representing H field - Linearised")
-endif
+ylabel("Transmission (S_{21}) representing H field - Linearised")
 title("Plot of H Field strength for different probe positions (y ~ 0 mm, z ~5 mm)")
 grid on
 legend('FontSize',11)
