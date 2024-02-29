3978b47af5958f69a406460e43ca6af0566f297d
Thomas Sharratt <thomassharratt@hotmail.co.uk>
2024-02-29 12:45:29 +0000

    multi, added folder and file sweep, added STANDARD_SWEEP
    now also works for z sweep files generated by hfield.py

--- plot_hfield_multi.m
+++ plot_hfield_multi.m
@@ -1,43 +1,66 @@
 clear all
 close all
 
-SAVE_IMG = false;
-FILE_NAME = "xsweep_0_40_21.csv";
-FOLDER_NAME = ["Coil A";
-               "Coil C"];
-##FOLDER_NAME = ["Coil C";
+SAVE_IMG = true;
+SWEEP_TYPE = 'z';
+##FILE_NAME = "xsweep_0_40_21.csv";
+FILE_NAME = ["zsweepx0_4_87_8.csv";
+             "zsweepx10_4_87_8.csv";
+             "zsweepx20_4_87_8.csv"];
+##FOLDER_NAME = ["Coil A";
+##               "Coil C"];
+FOLDER_NAME = ["Coil A"];
 ##               "Coil C 2"];
+##               "Coil C 3"];
+STANDARD_SWEEP = false; % true if using linspace, false if arbitrary list of points
+% if not, fill in x below accordingly
+##TITLE = "Plot of H Field strength for different probe positions (y ~ 0 mm, z ~5 mm)";
+TITLE = "Z sweep measurement of coil A at x = 0, 10, 20 mm";
 
 
-sweepDetailsCell = strsplit(strtok(FILE_NAME,'.'),'_');
-sweepDetailsCell = sweepDetailsCell(2:end);
+if (STANDARD_SWEEP) % linspace extracted from filename data
+  sweepDetailsCell = strsplit(strtok(FILE_NAME,'.'),'_');
+  sweepDetailsCell = sweepDetailsCell(2:end)
 
-sweepDetailsMat = [];
+  sweepDetailsMat = [];
 
-for i = 1:3
-  sweepDetailsMat(i) = str2num(cell2mat(sweepDetailsCell(i)));
-endfor
+  for i = 1:3
+    sweepDetailsMat(i) = str2num(cell2mat(sweepDetailsCell(i)));
+  endfor
 
-x = linspace(sweepDetailsMat(1), sweepDetailsMat(2), sweepDetailsMat(3));
+  x = linspace(sweepDetailsMat(1), sweepDetailsMat(2), sweepDetailsMat(3));
+else
+  % manual point entry
+  x = [4, 17, 27, 37, 50, 67, 77, 87];
+endif
 
 figure()
 hold on
 
 for i = 1:size(FOLDER_NAME,1)
-  data = csvread([strtrim(FOLDER_NAME(i,:)),filesep, "Hfield", filesep, FILE_NAME]);
+  for j = 1:size(FILE_NAME,1)
+    data = csvread([strtrim(FOLDER_NAME(i,:)),filesep, "Hfield", filesep, ...
+                    strtrim(FILE_NAME(j,:))]);
 
-  plot(x, power(10, data/20), '-x', 'DisplayName', FOLDER_NAME(i,:))
+    plot(x, power(10, data/20), '-x', ...
+         'DisplayName', [FOLDER_NAME(i,:), ' - ', FILE_NAME(j,:)])
+  endfor
 endfor
 
-xlabel("x [mm]")
+xlabel([SWEEP_TYPE, " [mm]"])
 ylabel("Transmission (S_{21}) representing H field - Linearised")
-title("Plot of H Field strength for different probe positions (y ~ 0 mm, z ~5 mm)")
+title(TITLE)
 grid on
-legend('FontSize',11)
+legend('FontSize',11, 'Interpreter', 'none')
 
 if (SAVE_IMG)
-  saveImages(["images", filesep],
-              ["overlay_xsweep_", num2str(sweepDetailsMat(1)), "_", ...
+  if (STANDARD_SWEEP)
+    saveImages(["images", filesep],
+              ["overlay_", SWEEP_TYPE, "sweep_", num2str(sweepDetailsMat(1)), "_", ...
               num2str(sweepDetailsMat(2)), "_", ...
               num2str(sweepDetailsMat(3)), " "])
+  else
+    saveImages(["images", filesep],
+              ["overlay_", SWEEP_TYPE, "sweep_"])
+  endif
 endif
