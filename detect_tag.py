4450ca346f19714d78e6138144c42af2deb9f083
Thomas Sharratt <thomassharratt@hotmail.co.uk>
2024-03-09 12:05:09 +0000

    comments & tidy up

--- detect_tag.py
+++ detect_tag.py
@@ -1,23 +1,31 @@
+"""
+Script to interface with Keysight DSOX3014A Scope and Arduino uno running the "fyp_reader_control" repo.
 
+Writes to Arduino (via serial) which produces modulated 13.56 MHz to a reader antenna. This is then recorded by a scope (controlled & processed here)
+
+Keysight programming reference: https://www.keysight.com/us/en/assets/9018-06894/programming-guides/9018-06894.pdf
+"""
 
 import serial
 
 COM_PORT = "/dev/ttyACM1"
-
 SER_MESSAGE = b"AZZXXYZXYZYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYZZXXYZXYZYYYY\n"
+# b'' notation needed ^
+
+# Setup Scope
 
 
-ser_arduino = serial.Serial(COM_PORT, 115200)
+# Send data
+ser_arduino = serial.Serial(COM_PORT, 115200)  # no timeout specified, breaks stuff
 
+print(ser_arduino.readline())  # prints "Serial Initialised."
 
-print(ser_arduino.readline())
+ser_arduino.write(SER_MESSAGE)  # write message
 
-ser_arduino.write(SER_MESSAGE)
-
-# don't bother reading it all back.
+# don't bother reading serial (Arduino sends message back)
 # while True:
 #     print(ser_arduino.readline())
 
-ser_arduino.close()
+ser_arduino.close() # finished with arduino.
 
 
