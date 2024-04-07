226216749aab04d002ce644461ff25533cb683de
Thomas Sharratt <thomassharratt@hotmail.co.uk>
2024-04-07 16:03:46 +0100

    RESTRUCTURED file system - put each device into own folder

---
+++ Keysight DSOX3014A Scope/saveImages.m
@@ -0,0 +1,18 @@
+
+function saveImages(folder, filePrefix)
+  % saves all open figures to images/ directory with timestamp and figure number
+  if (!exist(folder)) % creates directory if doesn't exist
+    disp([folder, " directory doesn't exist, creating..."])
+    mkdir(folder);
+  endif
+
+  figHandles = get(groot, 'Children');
+  currTime = datestr(clock, 'yy-mm-dd HH-MM-SS');
+  for i = 1:size(figHandles,1)
+    imfile = [folder, filePrefix, currTime,' Fig', num2str(figHandles(i,:)), '.png'];
+    saveas(figHandles(i,:),imfile)
+  endfor
+endfunction
+
+% function file saveImages created by Thomas Sharratt Copyright (C) 2024
+% <Created for Octave on Arch Linux & Windows>
