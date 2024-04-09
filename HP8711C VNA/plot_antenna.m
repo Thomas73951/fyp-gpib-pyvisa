7bcfd31b87987dd9313f6d2d719d6aa02d40f117
Thomas Sharratt <thomassharratt@hotmail.co.uk>
2024-04-09 15:52:59 +0100

    added specific plotter for antenna.py
    derived from plot_result.m

---
+++ HP8711C VNA/plot_antenna.m
@@ -0,0 +1,37 @@
+clc
+clear all
+close all
+
+% Simple file plotting, designed for S21 or S11 VNA plots.
+% Option to save figure to disk.
+
+SAVE_IMG = true;
+FILE_NAME = "data_1601_MEAS1.csv";
+FOLDER_NAME = "antenna/Coil A/2"; % file ^ in this folder, no filesep at end.
+
+
+data = csvread([FOLDER_NAME, filesep, FILE_NAME]);
+
+figure()
+f = linspace(13.5-1, 13.5+1, 1601)*1e6;
+##f = linspace(1, 1601, 1601)*1e6;
+
+minData = min(data);
+minIdx = find(data == minData);
+minFreq = f(minIdx);
+
+plot(f/1e6, data, 'DisplayName', 'S11')
+hold on
+##plot([13.56, 13.56], [0 -30], '--k', 'DisplayName', 'f0 (13.56 MHz)')
+text((minFreq+10e3)/1e6, minData-0.4, ...
+        ["Minimum @ ", num2str(minFreq/1e6), " MHz, ", num2str(minData), " dB"])
+xlabel("Frequency [MHz]")
+ylabel("Scattering Parameter [dB]")
+xlim([min(f), max(f)]/1e6)
+##ylim([-60 0])
+legend()
+grid on
+
+if (SAVE_IMG)
+  saveImages([FOLDER_NAME, filesep], "")
+endif
