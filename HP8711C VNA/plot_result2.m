f5458bd04b4a23f9f2d82ce4018a39f215eda252
Thomas Sharratt <thomassharratt@hotmail.co.uk>
2024-04-17 19:12:39 +0100

    vna added plotresult multi + edit on result2

--- HP8711C VNA/plot_result2.m
+++ HP8711C VNA/plot_result2.m
@@ -4,19 +4,19 @@ close all
 
 % Alternative version of plot_result.m
 
-SAVE_IMG = true;
-START_FREQ = 13e6;
-STOP_FREQ = 15e6;
+SAVE_IMG = false;
+START_FREQ = 10e6;
+STOP_FREQ = 17e6;
 MEAS = ["2";"1"];
 ##FOLDER_NAME = ["Coil C", filesep, "5"]; % file ^ in this folder
-FOLDER_NAME = ["vna", filesep, "rxfilt_12mar", filesep, "13_15"];
-
+FOLDER_NAME = ["tw", filesep, "line", filesep, "Coil A", filesep, "2", ...
+               filesep, "1"];
 
 figure()
-f = linspace(START_FREQ, STOP_FREQ, 801);
+f = linspace(START_FREQ, STOP_FREQ, 1601);
 hold on
 for i = 1:size(MEAS,1)
-  FILE_NAME = ["data_801_MEAS", MEAS(i), ".csv"];
+  FILE_NAME = ["data_1601_MEAS", MEAS(i), ".csv"];
   data = csvread([FOLDER_NAME, filesep, FILE_NAME]);
   plot(f/1e6, data, 'DisplayName', ['S', MEAS(i), '1'])
 endfor
@@ -24,6 +24,7 @@ endfor
 xlabel("Frequency [MHz]")
 ylabel("Scattering Parameter [dB]")
 xlim([min(f), max(f)]/1e6)
+ylim([-30 0])
 legend()
 grid on
 
