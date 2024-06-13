edceb77eac0c6dc9bef094073b519150196f0475
Thomas Sharratt <thomassharratt@hotmail.co.uk>
2024-06-13 09:08:53 +0100

    better ant plotting

--- HP8711C VNA/plot_antenna.m
+++ HP8711C VNA/plot_antenna.m
@@ -5,9 +5,20 @@ close all
 % Simple file plotting, designed for S21 or S11 VNA plots.
 % Option to save figure to disk.
 
-SAVE_IMG = true;
+SHOW_NOTCH_TEXT = false;
+PLOT_FC_FS = true;
+SAVE_IMG = false;
+YLIM = [-50 0];
 FILE_NAME = "data_1601_MEAS1.csv";
 FOLDER_NAME = "antenna/Coil A/2"; % file ^ in this folder, no filesep at end.
+TITLE = "Coil A";
+##FOLDER_NAME = "antenna/Coil A 1W/1";
+##TITLE = "Coil A 1W";
+##FOLDER_NAME = "antenna/Coil C/1";
+##TITLE = "Coil C";
+##FOLDER_NAME = "Coil C 1W/10";
+##TITLE = "Coil C 1W";
+
 
 
 data = csvread([FOLDER_NAME, filesep, FILE_NAME]);
@@ -20,17 +31,25 @@ minData = min(data);
 minIdx = find(data == minData);
 minFreq = f(minIdx);
 
-plot(f/1e6, data, 'DisplayName', 'S11')
+plot(f/1e6, data, 'DisplayName', 'S_{11}')
 hold on
 ##plot([13.56, 13.56], [0 -30], '--k', 'DisplayName', 'f0 (13.56 MHz)')
-text((minFreq+10e3)/1e6, minData-0.4, ...
-        ["Minimum @ ", num2str(minFreq/1e6), " MHz, ", num2str(minData), " dB"])
+if (SHOW_NOTCH_TEXT)
+  text((minFreq+10e3)/1e6, minData-0.4, ...
+          ["Minimum @ ", num2str(minFreq/1e6), " MHz, ", num2str(minData), " dB"])
+endif
+legend('location', 'southwest', 'Autoupdate', 'off')
+if (PLOT_FC_FS)
+  plot([12.7 12.7], YLIM, ':k')
+  plot([13.56 13.56], YLIM, '--k')
+  plot([14.4 14.4], YLIM, ':k')
+endif
 xlabel("Frequency [MHz]")
-ylabel("Scattering Parameter [dB]")
+ylabel("Amplitude [dB]")
 xlim([min(f), max(f)]/1e6)
-##ylim([-60 0])
-legend()
+ylim(YLIM)
 grid on
+title(TITLE)
 
 if (SAVE_IMG)
   saveImages([FOLDER_NAME, filesep], "")
